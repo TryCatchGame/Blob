e78e23b5e32f1b8d89a7a86e0f0f95a3a04b67a3
Kaynn Cahya <kaynncahya@outlook.com>
2019-04-13 20:02:13 +0900

    Updated in-game User Interface

--- Blob/Assets/Scripts/Managers/SceneHandler.cs
+++ Blob/Assets/Scripts/Managers/SceneHandler.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneHandler : Singleton<SceneHandler> {
+
+    [SerializeField]
+    private int playingFieldBuildIndex;
+
+    [SerializeField]
+    private int mainMenuBuildIndex;
+
     public void RestartScene() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
     }
+
+    public void GotoPlayingFieldScene() {
+        SceneManager.LoadScene(playingFieldBuildIndex);
+    }
+
+    public void GotoMainMenuScene() {
+        SceneManager.LoadScene(mainMenuBuildIndex);
+    }
+}
